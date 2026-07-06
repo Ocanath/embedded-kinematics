6ef0d48d0f7c07889e75a235368ff62f2bb4b972
ocanath <ocanath@gmail.com>
2026-07-06 14:33:04 -0700

    more scratch work basically

--- src/kinematics.h
+++ src/kinematics.h
@@ -1,18 +1,35 @@
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
 
 #include <stdint.h>
 #include <stddef.h>
 
-enum {JOINT_FIXED, JOINT_ROTATIONAL, JOINT_TRANSLATIONAL};
+typedef enum {JOINT_FIXED, JOINT_ROTATIONAL, JOINT_TRANSLATIONAL} joint_type_t;
 
+typedef struct quat_f_t
+{
+	float a;
+	float b;
+	float c;
+	float d;
+}quat_f_t;
 
+typedef struct vect3_t
+{
+	float x;
+	float y;
+	float z;
+}vect3_t;
 
 typedef struct link_t
 {
 	const char * name;	
+	//add information
 }link_t;
 
 typedef struct joint_t
 {
+	joint_type_t type;
 	link_t * parent;
 
 	link_t * children;
@@ -20,4 +37,9 @@ typedef struct joint_t
 
 	float q;
 
+	quat_f_t rotation;
+	vect3_t translation;
+
 }joint_t;
+
+#endif
