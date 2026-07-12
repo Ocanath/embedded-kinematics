196bae7ee01cc1e7fca4eb88907b16a4bbcc2da1
ocanath <ocanath@gmail.com>
2026-07-11 21:49:09 -0700

    templating out shit

--- src/kinematics.c
+++ src/kinematics.c
@@ -1,6 +1,23 @@
 #include "kinematics.h"
 #include <stdbool.h>
 
+typedef struct joint_stack_t
+{
+	joint_dual_quat_t * joints;
+	size_t num_joints;
+}joint_stack_t;
+
+
+int joint_pop(joint_stack_t * stack)
+{
+
+}
+
+int joint_push(joint_dual_quat_t * stack)
+{
+
+}
+
 
 int kin_dfs_dq(link_dual_quat_t * root)
 {
@@ -12,7 +29,7 @@ int kin_dfs_dq(link_dual_quat_t * root)
 			joint_dual_quat_t * j = &root->joints[joint_idx];
 			if(j->child == NULL)
 			{
-				
+
 			}
 		}
 	}
